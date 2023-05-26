5e9ea75c4b1422766451988b20452c9c8ea2d113
edmundnwl <ngwledmund@gmail.com>
2023-05-26 11:01:16 +0800

    attempt 2

---
+++ react-firebase-login/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import Auth from './components/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+      <SignUp />
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
