5c3b6b007b13b0c2260132133a6829997ba65fbb
kenurfseet@gmail.com <kenurfseet@gmail.com>
2023-06-10 18:32:03 +0800

    added input component, added signup checks

---
+++ react-firebase-login/src/components/Input.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Form } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Input(props) {
+  return(
+      <Form.Group className={props.className} controlId={props.controlId}>
+        <Form.Label>{props.text}</Form.Label>
+        <Form.Control 
+          type={props.type}
+          placeholder={props.placeholder}
+          value={props.value}
+          onChange={props.onChange}/>
+      </Form.Group>
+  );
+}
+
+export default Input;
