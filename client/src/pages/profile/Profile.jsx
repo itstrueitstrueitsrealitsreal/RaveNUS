6ffca7a3deb6a234bf20fe6e4c289cc692fd177a
edmundnwl <ngwledmund@gmail.com>
2023-06-19 00:27:54 +0800

    profile exists

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Auth from "../../components/auth/Auth";
 import Navbar from "../../components/Navbar";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import UserID from "../../components/auth/UserID";
+import { db } from "../../components/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function Profile(props) {
   console.log("Profile Page called");
 
+  // page navigation
   const navigate = useNavigate();
   const navigateToChangePassword = () => {
     navigate('/changepassword');
   }
 
+  // current userID
+  const uid = UserID();
+
+  // profile collection
+  const profileCollectionRef = collection(db, "profile");
+
+  // check if user profile exists
+  const [profs, setProfs] = useState([]);
+
+  useEffect(() => {
+    const checkProfile = async () => {
+      const profiles = await getDocs(profileCollectionRef);
+      setProfs(profiles.docs.map((doc) => ({...doc.data()})));
+    }
+    checkProfile();
+  }, [])
+  
+  const profiles = profs.filter((p) => p.UserID === uid ? true : false).length;
+  
+
+  // Page content
   const cont = (
     <div>
       <h1>PROFILE PAGE</h1>
       <Auth />
 
       <br />
-      <Button onClick={navigateToChangePassword}>Change Password</Button>
+      <div>
+        <Button onClick={navigateToChangePassword}>Change Password</Button>
+      </div>
+      <br />
+
+      { profiles === 1 ? <p>profile exists</p> : <p>profile doesnt exist</p> }
+      
     </div>
   )
 
