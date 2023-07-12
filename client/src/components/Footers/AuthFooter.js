78fd5fe5bd1e212033cbb87694bfd0818533e3da
kenurfseet@gmail.com <kenurfseet@gmail.com>
2023-07-13 01:53:48 +0800

    fixed header, footer, navbar

--- client/src/components/Footers/AuthFooter.js
+++ client/src/components/Footers/AuthFooter.js
@@ -1,22 +1,3 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
-
 // reactstrap components
 import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
 
@@ -31,45 +12,30 @@ const Login = () => {
                 © {new Date().getFullYear()}{" "}
                 <a
                   className="font-weight-bold ml-1"
-                  href="https://www.creative-tim.com?ref=adr-auth-footer"
                   target="_blank"
                 >
-                  Creative Tim
+                  Kenneth Seet, Edmund Ng
                 </a>
               </div>
             </Col>
             <Col xl="6">
-              <Nav className="nav-footer justify-content-center justify-content-xl-end">
+              <Nav className="nav-footer justify-content-center justify-content-xl-start">
                 <NavItem>
                   <NavLink
-                    href="https://www.creative-tim.com?ref=adr-auth-footer"
+                    className="justify-content-center"
+                    href="https://docs.google.com/document/d/19blLvT9Qm1wjgwXpkjagxnkMIjLazTdj-sTa8llWuuc/edit?usp=sharing"
                     target="_blank"
                   >
-                    Creative Tim
+                    Documentation
                   </NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink
-                    href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
+                    className="justify-content-center"
+                    href="https://github.com/edmundnwl/RaveNUS"
                     target="_blank"
                   >
-                    About Us
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="http://blog.creative-tim.com?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    Blog
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    MIT License
+                    GitHub
                   </NavLink>
                 </NavItem>
               </Nav>
